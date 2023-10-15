15117ae111e45f2effd25ee2b5202aa9ad6be186
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-10-15 22:02:26 +0100

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This contains the amenity information for the class"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel) :
+    """ The Amenity class from BaseModel"""
+    name = ""
