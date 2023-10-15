b7a54bfb5b6194e16cba4c6c3a98afffd15003f5
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-10-15 22:17:26 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This contains the state information for the class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    The State class from BaseModel
+    """
+    name = ""
